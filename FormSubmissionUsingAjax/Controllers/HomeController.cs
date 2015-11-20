73e6e2f8020ecbf8871535df36d4d28b56c207c6
ead2015 <ead.shuja@gmail.com>
2015-11-20 19:45:57 +0500

    Commit-1

---
+++ FormSubmissionUsingAjax/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormSubmissionUsingAjax.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult SerializeExample()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string Save()
+        {
+            return "You have been Registered with following details, Name = " + Request["name"] + " Email = " + Request["email"] + " Message = " + Request["message"];
+        }
+    }
+}
